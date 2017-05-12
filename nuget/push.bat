a65869ecb6f273a2347624f773101f6d00f6aa66
nitacore <nitacore@outlook.com>
2017-05-12 22:34:41 +0900

    update nuget package

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,3 +1,3 @@
-nuget push NetStitch.Client.0.1.5.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push NetStitch.Server.0.1.5.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push NetStitch.Analyzer.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push NetStitch.Client.0.1.7.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push NetStitch.Server.0.1.7.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push NetStitch.Analyzer.0.1.1.nupkg -Source https://www.nuget.org/api/v2/package
